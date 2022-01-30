da303b481eeab7336a8781d84c9440b8c205e873
besmaerteralb <denis.mimini.1@outlook.com>
2022-01-30 19:03:12 +0100

    Start working with header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,32 @@
-// @ts-nocheck
+import { Link } from 'react-router-dom';
+
 import React from 'react';
+// @ts-ignore
 import logo from '../assets/logo.png'
 
 function Header() {
     return (
-        <div className='navbar'>
+        <header className='navbar'>
             <div className='leftSide'>
                 <img src={logo} alt="logo" />
             </div>
-            <div className='rightSide'></div>
-        </div>
+            <nav className='rightSide'>
+                <ul>
+                    <li>
+                        <Link to="/" >Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/modules" >Modules</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact" >Contact Us</Link></li>
+                    <li>
+                        <Link to="/about" >About</Link>
+                    </li>
+                </ul>
+
+            </nav>
+        </header >
     )
 }
 
