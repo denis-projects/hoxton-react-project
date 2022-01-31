8bd09edf60c507bb7c20a4814f99467ae3c9328f
besmaerteralb <denis.mimini.1@outlook.com>
2022-01-31 21:35:56 +0100

    adding some infos on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,27 @@ function About() {
         <div className='about'>
             <div className='aboutTop'>
                 <h1>About Us</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus possimus quae eveniet facere. Facere suscipit placeat quaerat iusto expedita. Fugit nulla illo consequuntur! Dignissimos cupiditate rerum blanditiis explicabo earum id.</p>
+                <p><b>The mission of Hoxton Academy is to build the next generation of software engineers not only changing young people’s lives but also helping businesses find and retain talent.
+                    With millions of businesses around the world increasing their digital transformation efforts, a big challenge lies ahead for the global economy, talent shortage.</b>
+
+
+                    Although a seductive and quite lucrative career, companies around the world cannot fill their needs for software engineers and the demand for talents in this field will go up to 85.2 million by 2030. Companies risk losing more than 8.4 trillion dollars because of talent shortage.
+
+                    But the reality is more complicated than simply a software engineer shortage. The problem has to do with the way most companies recruit these talents.
+
+                    Hoxton Academy, a project started as a close partnership between London, Tirana and Pristina, is coming to Albania thanks to ICTSmedia and Twig and it represents an innovative professional education methodology with immediate employment outcomes.
+
+                    The mission of Hoxton Academy is to build the next generation of software engineers not only changing young people’s lives but also helping businesses find and retain talent.
+
+                    Hoxton Academy is a company with a London HQ, focused on professional education and helping individuals start a new career in software development in only 6 months. These individuals will learn about coding, cooperation, communication and new skills that will help them become future professionals.
+
+                    A wide partnership between Hoxton Academy and companies such as Twig, Hajde, ICTSlab, OneUp, Baboon, Monochain and Laralara, which are committed to offering scholarships for students who are selected to become part of the academy.
+
+                    “Our mission is to make a better world through education. I’m very excited to be working on a curriculum and methodology that will not only teach students the latest technologies they need to know to thrive in the industry, but will ultimately shape them into great problem solvers and communicators, providing them with skills that will prove extremely valuable throughout the rest of their careers,” says Nicolas Marcora, Headmaster at Hoxton Academy.
+
+                    Marcora believes that Hoxton Academy training will also benefit companies greatly, providing them with a pool of talented young individuals, eager to learn and capable of adapting to overcome any problem.
+
+                    The industry standard today relies on a very simple recruitment process: applying for a job means going through the process of being interviewed, completing a CV or sharing experience. If a software engineer has not pursued an education at an elite university, they are faced with the challenge of proving their talent. According to Stack Overflow data, 87% of software engineers say they have learned how to program by themselves.</p>
             </div>
             <div className='aboutBottom'>
                 <h2>Testimonials</h2>
