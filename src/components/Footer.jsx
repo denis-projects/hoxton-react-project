0d6fae49b6b99306410812f05ea55f1d48587527
besmaerteralb <denis.mimini.1@outlook.com>
2022-01-31 18:59:05 +0100

    adding the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import InstagramIcon from '@material-ui/icons/Instagram'
 import FacebookIcon from '@material-ui/icons/Facebook'
 import Linkedin from '@material-ui/icons/LinkedIn'
+import '../styles/Footer.css'
 
 function Footer() {
     return (
-        <div className='Footer'>
+        <div className='footer'>
             <div className='socialMedia'>
                 <InstagramIcon />
                 <FacebookIcon />
