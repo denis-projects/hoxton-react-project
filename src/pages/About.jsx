d0a03755aca08ff76e9adf09a21dbe7c5a3a32ac
besmaerteralb <denis.mimini.1@outlook.com>
2022-01-31 20:46:31 +0100

    adding some lorem ipsum to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 function About() {
-    return <div>
-        <h1>About Page</h1>
-    </div>;
-}
+    return (
+        <div className='about'>
+            <div className='aboutTop'>
+                <h1>About Us</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus possimus quae eveniet facere. Facere suscipit placeat quaerat iusto expedita. Fugit nulla illo consequuntur! Dignissimos cupiditate rerum blanditiis explicabo earum id.</p>
+            </div>
+            <div className='aboutBottom'>
+                <h2>Testimonials</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur, facere sunt quia iste a ut labore! Illo nobis explicabo quasi, esse necessitatibus aspernatur itaque corrupti nisi, voluptates commodi ut expedita.</p>
+            </div>
 
+        </div>
+    )
+
+}
 export default About;
