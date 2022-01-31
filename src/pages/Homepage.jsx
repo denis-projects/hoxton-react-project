de91fab19bb62a12034aaddf95672d4268661d00
besmaerteralb <denis.mimini.1@outlook.com>
2022-01-31 12:09:19 +0100

    adding background image on the main page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import '../styles/HomePage.css'
+// @ts-ignore
+import MainImage from '../assets/hoxton_class.jpg'
 
 function Homepage() {
-    return <div>
-        <h1>Home</h1>
-    </div>;
+    return (
+        <div className='home'>
+            <div className='headerContainer' style={{ backgroundImage: `url(${MainImage})` }} >
+                <h1>Next generation of Software Engineers</h1>
+                {/* <p>The best place for a life changing experience</p> */}
+                <Link to="/contact">
+                    <button> APPLY NOW </button>
+                </Link>
+
+            </div>
+        </div >
+    )
 }
 
 export default Homepage;
