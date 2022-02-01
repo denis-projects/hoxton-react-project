26facb3d533a1bd6103270efcb2b3812ee4279d3
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-01 21:26:58 +0100

    adding module description

--- src/pages/ModuleDetails.jsx
+++ src/pages/ModuleDetails.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function ModuleDetails() {
+
+    const [module, setModule] = useState(null)
+
     const params = useParams()
+
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/modules/${params.id}`)
+            .then(resp => resp.json())
+            .then(modulesFromServer => setModule(modulesFromServer))
+
+    }, [])
+
+    if (module === null) return <h2>Please wait</h2>
+
     return <div>
-        <h1>Module Details: {params.id}</h1>
+        <section className='module-detail main-wraper'>
+            <img src={module.image} alt={module.title} />
+            <div className='product-detail__side'>
+                <h2>{module.name}</h2>
+                <p>{module.description}</p>
+                <button>Apply</button>
+
+            </div>
+        </section>
     </div>;
 }
 
