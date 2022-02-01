cd17003ee31b03fd80b218085b805af258aec397
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-01 22:53:44 +0100

    .

--- src/pages/Modules.jsx
+++ src/pages/Modules.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useEffect, useState } from "react"
-import ModuleList from '../components/ModuleList';
+// import ModuleList from '../components/ModuleList';
 import '../styles/Modules.css'
+import { Link } from 'react-router-dom'
 
 function Modules() {
 
@@ -16,7 +17,21 @@ function Modules() {
     return (
         <div className='modules'>
             <h1 className='modulesTitle'>The Modules</h1>
-            <ModuleList modules={modules} />
+            {/* <ModuleList modules={modules} /> */}
+            <ul className='modulesList'>
+                {modules.map(module => (
+                    // <MolduleItem key={module.id} module={module} />
+                    <li>
+                        <Link to={`/modules/${module.id}`}>
+                            <div className='modulesItem'>
+                                <img src={module.image} alt={module.name} />
+                                <h3>{module.name}</h3>
+                                <p>{module.weekDuration}</p>
+                            </div>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div >
     )
 }
