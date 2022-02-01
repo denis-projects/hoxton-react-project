6ab08b946ed5a2a0bacec300abd4c7dbf61af6fe
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-01 22:53:27 +0100

    finished the day

--- src/components/ModuleList.jsx
+++ src/components/ModuleList.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import MolduleItem from './MolduleItem';
+// import React from 'react';
+// import Modules from '../pages/Modules';
+// import MolduleItem from './MolduleItem';
 
-function ModuleList() {
-    return (
-        <ul className='modulesList'>
-            {modules.map(module => (
-                <MolduleItem key={module.id} module={module} />
-            ))}
-        </ul>
+// function ModuleList() {
 
-    )
-}
+//     if (Modules.length === 0) return <p>Sorry, we cannot find the modules</p>
 
-export default ModuleList;
+//     return (
+//         <ul className='modulesList'>
+//             {modules.map(module => (
+//                 <MolduleItem key={module.id} module={module} />
+//             ))}
+//         </ul>
+
+//     )
+// }
+
+// export default ModuleList;
--- src/components/MolduleItem.jsx
+++ src/components/MolduleItem.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+// import React from 'react';
+// import { Link } from 'react-router-dom'
 
-function MolduleItem({ module }) {
+// function MolduleItem({ module }) {
+//     if (module === null) return <p>Loading</p>
 
+//     if (module.id === undefined) return <p>No modules</p>
 
-    return (
-        <li>
-            <Link to={`/modules/${module.id}`}>
-                <div className='modulesItem'>
-                    <img src={module.image} alt={module.name} />
-                    <h3>{module.name}</h3>
-                    <p>{module.weekDuration}</p>
-                </div>
-            </Link>
-        </li>
-    )
-}
+//     return (
+//         <li>
+//             <Link to={`/modules/${module.id}`}>
+//                 <div className='modulesItem'>
+//                     <img src={module.image} alt={module.name} />
+//                     <h3>{module.name}</h3>
+//                     <p>{module.weekDuration}</p>
+//                 </div>
+//             </Link>
+//         </li>
+//     )
+// }
 
-export default MolduleItem;
+// export default MolduleItem;
