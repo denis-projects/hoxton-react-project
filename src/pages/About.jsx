bf1faa2df8f737dcfcc5eaa8be8cf05b6f2c0279
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-02 14:36:00 +0100

    adding some more styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,18 +9,22 @@ function About() {
                 <img src='assets\Homepage Photo.jpeg' alt="" />
                 <p><b>The mission of Hoxton Academy is to build the next generation of software engineers not only changing young people’s lives but also helping businesses find and retain talent.
                     With millions of businesses around the world increasing their digital transformation efforts, a big challenge lies ahead for the global economy, talent shortage.</b> </p>
-                <p>Although a seductive and quite lucrative career, companies around the world cannot fill their needs for software engineers and the demand for talents in this field will go up to 85.2 million by 2030. Companies risk losing more than 8.4 trillion dollars because of talent shortage. But the reality is more complicated than simply a software engineer shortage. The problem has to do with the way most companies recruit these talents. Hoxton Academy, a project started as a close partnership between London, Tirana and Pristina, is coming to Albania thanks to ICTSmedia and Twig and it represents an innovative professional education methodology with immediate employment outcomes.</p>
-                <p>The mission of Hoxton Academy is to build the next generation of software engineers not only changing young people’s lives but also helping businesses find and retain talent.Hoxton Academy is a company with a London HQ, focused on professional education and helping individuals start a new career in software development in only 6 months. These individuals will learn about coding, cooperation, communication and new skills that will help them become future professionals.</p>
+
+
+                <p>Hoxton Academy, a project started as a close partnership between London, Tirana and Pristina, is coming to Albania thanks to ICTSmedia and Twig and it represents an innovative professional education methodology with immediate employment outcomes.</p>
+                <p>The mission of Hoxton Academy is to build the next generation of software engineers not only changing young people’s lives but also helping businesses find and retain talent.Hoxton Academy is a company with a London HQ, focused on professional education and helping individuals start a new career in software development in only 6 months.</p>
+                <p> These individuals will learn about coding, cooperation, communication and new skills that will help them become future professionals.</p>
                 <p>A wide partnership between Hoxton Academy and companies such as Twig, Hajde, ICTSlab, OneUp, Baboon, Monochain and Laralara, which are committed to offering scholarships for students who are selected to become part of the academy.</p>
 
-                <h2>Our Teachers</h2>
+                <h2>Our Headmaster Teacher</h2>
                 <h3>Nicolas Marcora</h3>
                 <p>“Our mission is to make a better world through education. I’m very excited to be working on a curriculum and methodology that will not only teach students the latest technologies they need to know to thrive in the industry, but will ultimately shape them into great problem solvers and communicators, providing them with skills that will prove extremely valuable throughout the rest of their careers,” says Nicolas Marcora, Headmaster at Hoxton Academy.</p>
                 <p> Marcora believes that Hoxton Academy training will also benefit companies greatly, providing them with a pool of talented young individuals, eager to learn and capable of adapting to overcome any problem.</p>
             </div>
             <div className='aboutBottom'>
                 <h2>Testimonials</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur, facere sunt quia iste a ut labore! Illo nobis explicabo quasi, esse necessitatibus aspernatur itaque corrupti nisi, voluptates commodi ut expedita.</p>
+                <h3>Denis Mimini (Student on first Cohort)</h3>
+                <p></p>
             </div>
 
         </div>
