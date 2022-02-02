731d4b27986cd8ce98f2c7939221bc09f8107f6c
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-02 12:23:37 +0100

    .

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,46 +1,17 @@
 import React from 'react';
+import Form from '../components/Form';
 import '../styles/Contact.css'
 
+
+
 function Contact() {
+
+
+
     return (
         <div className='contact'>
             <h1>Apply down bealow to take the Logic test today</h1>
-            <form id='formEl'
-                onSubmit={function (event) {
-                    event.preventDefault()
-                }}
-            >
-                <label htmlFor="">
-                    Name
-                    <input
-                        type="text"
-                        name='name'
-                        placeholder='Enter your name' />
-                </label>
-                <label htmlFor="">
-                    Last name
-                    <input
-                        type="text"
-                        name='lastname'
-                        placeholder='Enter your last name' />
-                </label>
-                <label htmlFor="">
-                    Email
-                    <input
-                        type="email"
-                        name='email'
-                        placeholder='Enter your email' />
-                </label>
-                <label htmlFor="">
-                    Add a comment
-                    <input
-                        type="text"
-                        name='comment'
-                        placeholder='Enter your email' />
-                </label>
-
-                <button type='submit' >Submit your Application</button>
-            </form>
+            <Form />
         </div>
     )
 }
