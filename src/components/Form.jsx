065618050f8d471adacaf4def748faf58c41b92e
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-02 23:27:39 +0100

    workimg with form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,25 +2,32 @@ import React, { useState } from 'react';
 
 export default function Form() {
 
-    const initialValues = {
-        name: "",
-        lastname: "",
-        email: "",
-        comment: ""
+    // const initialValues = {
+    //     name: "",
+    //     lastname: "",
+    //     email: "",
+    //     comment: ""
+    // }
+
+    // const [formValues, setFormValues] = useState(initialValues)
+
+    // const handleChange = (e) => {
+    //     const { name, value } = e.target
+    //     setFormValues({ ...formValues, [name]: value })
+
+    // }
+
+    function createUser(name, lastname, email, comment) {
+        fetch('http://localhost:3001/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+
+            },
+            body: JSON.stringify({ name, lastname, email, comment })
+        }).then(resp => resp.json())
     }
 
-    const [formValues, setFormValues] = useState(initialValues)
-
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setFormValues({ ...formValues, [name]: value })
-
-    }
-
-    const handleSubmit = (e) => {
-        e.prevenDefault()
-
-    }
 
 
 
@@ -28,7 +35,15 @@ export default function Form() {
         <>
 
             <form className='form'
-                onSubmit={handleSubmit}
+                onSubmit={function (event) {
+                    event.preventDefault()
+                    const name = event.target.name.value
+                    const lastname = event.target.lastname.value
+                    const email = event.target.email.value
+                    const comment = event.target.comment.value
+
+                    createUser(name, lastname, email, comment)
+                }}
             >
 
                 <div className='form-inputs'>
@@ -38,9 +53,9 @@ export default function Form() {
                             id='name'
                             type="text"
                             name='name'
-                            value={formValues.name}
+                            // value={formValues.name}
                             placeholder='Enter your name'
-                            onChange={handleChange}
+                        // onChange={handleChange}
                         />
                     </label>
                 </div>
@@ -51,9 +66,9 @@ export default function Form() {
                             id='lastname'
                             type="text"
                             name='lastname'
-                            value={formValues.lastname}
+                            // value={formValues.lastname}
                             placeholder='Enter your last name'
-                            onChange={handleChange}
+                        // onChange={handleChange}
                         />
                     </label>
                 </div>
@@ -64,9 +79,9 @@ export default function Form() {
                             id='email'
                             type="email"
                             name='email'
-                            value={formValues.email}
+                            // value={formValues.email}
                             placeholder='Enter your email'
-                            onChange={handleChange}
+                        // onChange={handleChange}
                         />
                     </label>
                 </div>
@@ -77,9 +92,9 @@ export default function Form() {
                             id='comment'
                             type="text"
                             name='comment'
-                            value={formValues.comment}
+                            // value={formValues.comment}
                             placeholder='Enter your comment'
-                            onChange={handleChange}
+                        // onChange={handleChange}
                         />
                     </label>
                 </div>
