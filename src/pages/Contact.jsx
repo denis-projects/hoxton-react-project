e65ac415785285b9c5d89c8911df77410d4e96f3
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-02 14:48:10 +0100

    add some more styling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Form from '../components/Form';
 import '../styles/Contact.css'
-
+import FormPhoto from '../../assets/formPhoto.jpg'
 
 
 
@@ -12,11 +12,12 @@ function Contact() {
     return (
         <div className='contact'>
 
-            <div className='form-content-left'>
-                <img className="form-img" src="/assets/formPhoto.jpg" alt="" />
+            <div className='form-content-left' style={{ backgroundImage: `url(${FormPhoto})` }} >
+                {/* <img className="form-img" src="/assets/formPhoto.jpg" alt="" /> */}
+                <Form />
             </div>
 
-            <Form />
+
         </div>
     )
 }
