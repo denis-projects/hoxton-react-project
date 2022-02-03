a5eafb6e6723060923f44828d3dce4090a1a9e8b
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-03 10:06:23 +0100

    testing

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Testimonials() {
+    return (
+        <div className='testimonials'>
+            <div className='testimonial'>
+                <img src="" alt="" />
+                <p className="testimonialText"></p>
+            </div>
+            <div className='testimonials'>
+                <div className='testimonial'>
+                    <img src="" alt="" />
+                    <p className="testimonialText"></p>
+                </div>
+            </div>
+            <div className='testimonials'>
+                <div className='testimonial'>
+                    <img src="" alt="" />
+                    <p className="testimonialText"></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Testimonials;
